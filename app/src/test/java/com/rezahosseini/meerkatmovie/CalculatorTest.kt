a0257247b5c657483d39a189e465f0713fcc3ef2
reza.h <reza.hd.he@gmail.com>
2026-09-07 00:25:39 +0330

    add phase3

--- app/src/test/java/com/rezahosseini/meerkatmovie/CalculatorTest.kt
+++ app/src/test/java/com/rezahosseini/meerkatmovie/CalculatorTest.kt
@@ -25,12 +25,44 @@ class CalculatorTest {
         assertEquals(5, resultAdd)
     }
     @Test
+    fun add_shouldReturnSum2() {
+
+
+        val resultAdd = calculator.add(5, 0)
+
+        assertEquals(5, resultAdd)
+    }
+    @Test
+    fun add_shouldReturnSum3() {
+
+
+        val resultAdd = calculator.add(-2, 3)
+
+        assertEquals(1, resultAdd)
+    }
+    @Test
     fun subtract_shouldReturnDifference() {
 
 
-        val resultSubtract = calculator.subtract(2, 3)
+        val resultSubtract = calculator.subtract(5, 2)
 
-        assertEquals(-1, resultSubtract)
+        assertEquals(3, resultSubtract)
+    }
+    @Test
+    fun subtract_shouldReturnDifference2() {
+
+
+        val resultSubtract = calculator.subtract(5, 0)
+
+        assertEquals(5, resultSubtract)
+    }
+    @Test
+    fun subtract_shouldReturnDifference3() {
+
+
+        val resultSubtract = calculator.subtract(2, 5)
+
+        assertEquals(-3, resultSubtract)
     }
     @Test
     fun multiply_shouldReturnProduct() {
@@ -41,12 +73,45 @@ class CalculatorTest {
         assertEquals(6, resultMultiply)
     }
     @Test
+    fun multiply_shouldReturnProduct2() {
+
+
+        val resultMultiply = calculator.multiply(5, 0)
+
+        assertEquals(0, resultMultiply)
+    }
+    @Test
+    fun multiply_shouldReturnProduct3() {
+
+
+        val resultMultiply = calculator.multiply(-2, 3)
+
+        assertEquals(-6, resultMultiply)
+    }
+
+    @Test
     fun divide_shouldReturnQuotient() {
 
 
-        val resultDivide= calculator.divide(4, 2)
+        val resultDivide= calculator.divide(2, 6)
 
-        assertEquals(2, resultDivide)
+        assertEquals(3, resultDivide)
+    }
+    @Test
+    fun divide_shouldReturnQuotient2() {
+
+
+        val resultDivide= calculator.divide(5, 1)
+
+        assertEquals(5, resultDivide)
+    }
+    @Test
+    fun divide_shouldReturnQuotient3() {
+
+
+        val resultDivide= calculator.divide(-6, 2)
+
+        assertEquals(-3, resultDivide)
     }
     @Test
     fun add_shouldReturnPositiveNumber() {
@@ -66,9 +131,9 @@ class CalculatorTest {
         assertNotNull(result)
     }
     @Test
-    fun multiplyAndAdd_shouldReturnToSame(){
-        val resultMultiply=calculator.multiply(2,2)
-        val resultAdd=calculator.add(2,2)
+    fun calculator_shouldReturnToSame(){
+        val resultMultiply=calculator
+        val resultAdd=calculator
         assertSame(resultAdd,resultMultiply)
     }
 }
